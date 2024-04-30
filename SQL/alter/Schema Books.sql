54afe5b25d8fe37eb06e8273a0a5f1ac32236d46
Francesco <ballerifrancesco@gmail.com>
2024-04-30 14:26:40 +0200

    add alter

--- SQL/alter/Schema Books.sql
+++ SQL/alter/Schema Books.sql
@@ -1,12 +1,11 @@
--- TABLE
 CREATE TABLE Books (
   book_id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
-  author TEXT NOT NULL,
+  author TEXT VARCHAR(1),
   genre TEXT NOT NULL,
   published_year INTEGER NOT NULL,
   isbn INTEGER NOT NULL,
   price INTEGER NOT NULL,
   rating INTEGER not NULL,
   stock_count INTEGER NOT NULL
-);
+, publisher text, number_of_pages integer)
