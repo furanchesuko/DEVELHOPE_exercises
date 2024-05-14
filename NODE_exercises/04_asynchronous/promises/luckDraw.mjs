8042f0322f0ba29c11015183aa182357fafa957b
Francesco <ballerifrancesco@gmail.com>
2024-05-14 13:57:22 +0200

    promise exe done

---
+++ NODE_exercises/04_asynchronous/promises/luckDraw.mjs
@@ -0,0 +1,39 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+        const win = Boolean(Math.round(Math.random()));
+
+        process.nextTick(() => {
+            if (win) {
+                resolve(`${player} won a prize in the draw!`);
+            } else {
+                reject(new Error(`${player} lost the draw.`));
+            }
+        });
+    });
+}
+
+luckyDraw('Joe')
+    .then(result => {
+        console.log(result);
+        return luckyDraw('Caroline');
+    })
+    .then(result => {
+        console.log(result);
+        return luckyDraw('Sabrina');
+    })
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.error("There has been an error", error);
+    });
+    
+//Un altro modo di scrivere la promise resolution
+
+/* luckyDraw('Joe')
+    .then((res) => console.log(res))
+    .then(() => luckyDraw('Caroline'))
+    .then((res) => console.log(res))
+    .then(() => luckyDraw('Sabrina'))
+    .then((res) => console.log(res))
+    .catch((err) => console.error("There has been an error", err)) */
